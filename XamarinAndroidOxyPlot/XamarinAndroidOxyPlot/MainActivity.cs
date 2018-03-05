c902e2204b149cfc6c38f85e09e5cce902c60134
Jcardif <josh.cardif14@outlook.com>
2018-03-05 22:21:47 +0300

    Plot a line graph

--- XamarinAndroidOxyPlot/XamarinAndroidOxyPlot/MainActivity.cs
+++ XamarinAndroidOxyPlot/XamarinAndroidOxyPlot/MainActivity.cs
@@ -12,28 +12,76 @@ namespace XamarinAndroidOxyPlot
     public class MainActivity : Activity
     {
         private PlotView view;
+        private Button btn1, btn2, btn3, btn4;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            view = FindViewById<PlotView>(Resource.Id.plot_view);
+            btn1 = FindViewById<Button>(Resource.Id.button1);
+            btn2 = FindViewById<Button>(Resource.Id.button2);
+            btn3 = FindViewById<Button>(Resource.Id.button3);
+            btn4 = FindViewById<Button>(Resource.Id.button4);
+            view = FindViewById<PlotView>(Resource.Id.oxyplotView1);
 
-            var plotModel = new PlotModel
+            btn1.Click += Btn1_Click;
+            btn2.Click += Btn2_Click;
+            btn3.Click += Btn3_Click;
+            btn4.Click += Btn4_Click;
+
+        }
+
+        private void Btn4_Click(object sender, System.EventArgs e)
+        {
+            view.Model = ViewModel4();
+        }
+
+        private PlotModel ViewModel4()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Btn3_Click(object sender, System.EventArgs e)
+        {
+            view.Model = ViewModel3();
+        }
+
+        private PlotModel ViewModel3()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Btn2_Click(object sender, System.EventArgs e)
+        {
+            view.Model = ViewModel2();
+        }
+
+        private PlotModel ViewModel2()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        private void Btn1_Click(object sender, System.EventArgs e)
+        {
+            view.Model = ViewModel1();
+        }
+
+        private PlotModel ViewModel1()
+        {
+            var plotModel=new PlotModel{Title = "Title Here"};
+            plotModel.Axes.Add(new LinearAxis{Position = AxisPosition.Bottom});
+            plotModel.Axes.Add(new LinearAxis {Position = AxisPosition.Left, Maximum = 10, Minimum = 0});
+
+            var series1 = new LineSeries()
             {
-                Title = "OxyPlot Demo"
+                Color = OxyColors.Yellow,
+                MarkerType = MarkerType.Star,
+                MarkerSize = 5,
+                MarkerStroke = OxyColors.White,
+                MarkerFill = OxyColors.Black,
+                MarkerStrokeThickness = 4.0
             };
-            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom});
-            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0 });
-
-            var series1 = new LineSeries
-            {
-                MarkerType = MarkerType.Circle,
-                MarkerSize = 4,
-                MarkerStroke = OxyColors.White
-            };
-
             series1.Points.Add(new DataPoint(0.0, 6.0));
             series1.Points.Add(new DataPoint(1.4, 2.1));
             series1.Points.Add(new DataPoint(2.0, 4.2));
@@ -43,6 +91,7 @@ namespace XamarinAndroidOxyPlot
             series1.Points.Add(new DataPoint(8.9, 8.9));
 
             plotModel.Series.Add(series1);
+            return plotModel;
         }
     }
 }
--- XamarinAndroidOxyPlot/XamarinAndroidOxyPlot/Resources/Resource.Designer.cs
+++ XamarinAndroidOxyPlot/XamarinAndroidOxyPlot/Resources/Resource.Designer.cs
@@ -2171,8 +2171,8 @@ namespace XamarinAndroidOxyPlot
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070085
-			public const int action0 = 2131165317;
+			// aapt resource value: 0x7f070089
+			public const int action0 = 2131165321;
 			
 			// aapt resource value: 0x7f070064
 			public const int action_bar = 2131165284;
@@ -2195,17 +2195,17 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070041
 			public const int action_bar_title = 2131165249;
 			
-			// aapt resource value: 0x7f070082
-			public const int action_container = 2131165314;
+			// aapt resource value: 0x7f070086
+			public const int action_container = 2131165318;
 			
 			// aapt resource value: 0x7f070065
 			public const int action_context_bar = 2131165285;
 			
-			// aapt resource value: 0x7f070089
-			public const int action_divider = 2131165321;
+			// aapt resource value: 0x7f07008d
+			public const int action_divider = 2131165325;
 			
-			// aapt resource value: 0x7f070083
-			public const int action_image = 2131165315;
+			// aapt resource value: 0x7f070087
+			public const int action_image = 2131165319;
 			
 			// aapt resource value: 0x7f070003
 			public const int action_menu_divider = 2131165187;
@@ -2222,11 +2222,11 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070043
 			public const int action_mode_close_button = 2131165251;
 			
-			// aapt resource value: 0x7f070084
-			public const int action_text = 2131165316;
+			// aapt resource value: 0x7f070088
+			public const int action_text = 2131165320;
 			
-			// aapt resource value: 0x7f070092
-			public const int actions = 2131165330;
+			// aapt resource value: 0x7f070096
+			public const int actions = 2131165334;
 			
 			// aapt resource value: 0x7f070044
 			public const int activity_chooser_view_content = 2131165252;
@@ -2252,11 +2252,23 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070028
 			public const int bottom = 2131165224;
 			
+			// aapt resource value: 0x7f070081
+			public const int button1 = 2131165313;
+			
+			// aapt resource value: 0x7f070082
+			public const int button2 = 2131165314;
+			
+			// aapt resource value: 0x7f070083
+			public const int button3 = 2131165315;
+			
+			// aapt resource value: 0x7f070084
+			public const int button4 = 2131165316;
+			
 			// aapt resource value: 0x7f07004b
 			public const int buttonPanel = 2131165259;
 			
-			// aapt resource value: 0x7f070086
-			public const int cancel_action = 2131165318;
+			// aapt resource value: 0x7f07008a
+			public const int cancel_action = 2131165322;
 			
 			// aapt resource value: 0x7f070030
 			public const int center = 2131165232;
@@ -2270,8 +2282,8 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f07005b
 			public const int checkbox = 2131165275;
 			
-			// aapt resource value: 0x7f07008e
-			public const int chronometer = 2131165326;
+			// aapt resource value: 0x7f070092
+			public const int chronometer = 2131165330;
 			
 			// aapt resource value: 0x7f070039
 			public const int clip_horizontal = 2131165241;
@@ -2327,8 +2339,8 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070021
 			public const int end = 2131165217;
 			
-			// aapt resource value: 0x7f070098
-			public const int end_padder = 2131165336;
+			// aapt resource value: 0x7f07009c
+			public const int end_padder = 2131165340;
 			
 			// aapt resource value: 0x7f07002a
 			public const int enterAlways = 2131165226;
@@ -2366,8 +2378,8 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070049
 			public const int icon = 2131165257;
 			
-			// aapt resource value: 0x7f070093
-			public const int icon_group = 2131165331;
+			// aapt resource value: 0x7f070097
+			public const int icon_group = 2131165335;
 			
 			// aapt resource value: 0x7f070025
 			public const int ifRoom = 2131165221;
@@ -2375,8 +2387,8 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070046
 			public const int image = 2131165254;
 			
-			// aapt resource value: 0x7f07008f
-			public const int info = 2131165327;
+			// aapt resource value: 0x7f070093
+			public const int info = 2131165331;
 			
 			// aapt resource value: 0x7f070000
 			public const int item_touch_helper_previous_elevation = 2131165184;
@@ -2387,11 +2399,11 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070034
 			public const int left = 2131165236;
 			
-			// aapt resource value: 0x7f070094
-			public const int line1 = 2131165332;
+			// aapt resource value: 0x7f070098
+			public const int line1 = 2131165336;
 			
-			// aapt resource value: 0x7f070096
-			public const int line3 = 2131165334;
+			// aapt resource value: 0x7f07009a
+			public const int line3 = 2131165338;
 			
 			// aapt resource value: 0x7f07000f
 			public const int listMode = 2131165199;
@@ -2399,11 +2411,11 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070048
 			public const int list_item = 2131165256;
 			
-			// aapt resource value: 0x7f07009a
-			public const int masked = 2131165338;
+			// aapt resource value: 0x7f07009e
+			public const int masked = 2131165342;
 			
-			// aapt resource value: 0x7f070088
-			public const int media_actions = 2131165320;
+			// aapt resource value: 0x7f07008c
+			public const int media_actions = 2131165324;
 			
 			// aapt resource value: 0x7f070022
 			public const int middle = 2131165218;
@@ -2426,14 +2438,17 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070010
 			public const int normal = 2131165200;
 			
-			// aapt resource value: 0x7f070091
-			public const int notification_background = 2131165329;
+			// aapt resource value: 0x7f070095
+			public const int notification_background = 2131165333;
 			
-			// aapt resource value: 0x7f07008b
-			public const int notification_main_column = 2131165323;
+			// aapt resource value: 0x7f07008f
+			public const int notification_main_column = 2131165327;
 			
-			// aapt resource value: 0x7f07008a
-			public const int notification_main_column_container = 2131165322;
+			// aapt resource value: 0x7f07008e
+			public const int notification_main_column_container = 2131165326;
+			
+			// aapt resource value: 0x7f070085
+			public const int oxyplotView1 = 2131165317;
 			
 			// aapt resource value: 0x7f070037
 			public const int parallax = 2131165239;
@@ -2443,9 +2458,6 @@ namespace XamarinAndroidOxyPlot
 			
 			// aapt resource value: 0x7f070038
 			public const int pin = 2131165240;
-			
-			// aapt resource value: 0x7f070081
-			public const int plot_view = 2131165313;
 			
 			// aapt resource value: 0x7f070006
 			public const int progress_circular = 2131165190;
@@ -2459,11 +2471,11 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070035
 			public const int right = 2131165237;
 			
-			// aapt resource value: 0x7f070090
-			public const int right_icon = 2131165328;
+			// aapt resource value: 0x7f070094
+			public const int right_icon = 2131165332;
 			
-			// aapt resource value: 0x7f07008c
-			public const int right_side = 2131165324;
+			// aapt resource value: 0x7f070090
+			public const int right_side = 2131165328;
 			
 			// aapt resource value: 0x7f07001a
 			public const int screen = 2131165210;
@@ -2558,8 +2570,8 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070036
 			public const int start = 2131165238;
 			
-			// aapt resource value: 0x7f070087
-			public const int status_bar_latest_event_content = 2131165319;
+			// aapt resource value: 0x7f07008b
+			public const int status_bar_latest_event_content = 2131165323;
 			
 			// aapt resource value: 0x7f07005e
 			public const int submenuarrow = 2131165278;
@@ -2570,11 +2582,11 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f070011
 			public const int tabMode = 2131165201;
 			
-			// aapt resource value: 0x7f070097
-			public const int text = 2131165335;
+			// aapt resource value: 0x7f07009b
+			public const int text = 2131165339;
 			
-			// aapt resource value: 0x7f070095
-			public const int text2 = 2131165333;
+			// aapt resource value: 0x7f070099
+			public const int text2 = 2131165337;
 			
 			// aapt resource value: 0x7f070052
 			public const int textSpacerNoButtons = 2131165266;
@@ -2591,8 +2603,8 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f07000d
 			public const int textinput_error = 2131165197;
 			
-			// aapt resource value: 0x7f07008d
-			public const int time = 2131165325;
+			// aapt resource value: 0x7f070091
+			public const int time = 2131165329;
 			
 			// aapt resource value: 0x7f07004a
 			public const int title = 2131165258;
@@ -2627,8 +2639,8 @@ namespace XamarinAndroidOxyPlot
 			// aapt resource value: 0x7f07000e
 			public const int view_offset_helper = 2131165198;
 			
-			// aapt resource value: 0x7f070099
-			public const int visible = 2131165337;
+			// aapt resource value: 0x7f07009d
+			public const int visible = 2131165341;
 			
 			// aapt resource value: 0x7f070027
 			public const int withText = 2131165223;
